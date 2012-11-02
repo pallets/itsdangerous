97fa108ce6844fa82e2e36edd83a80041462d11b
Puzzlet Chung <puzzlet@gmail.com>
2012-11-02 19:34:23 +0900

    2to3 available - passes tests with py25-27,py31-32,pypy

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+import sys
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 
+kwargs = {}
+if sys.version_info >= (3, ):
+    kwargs['use_2to3'] = True
 setup(
     name='itsdangerous',
     author='Armin Ronacher',
@@ -15,6 +19,14 @@ setup(
     zip_safe=False,
     classifiers=[
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python'
-    ]
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    **kwargs
 )
