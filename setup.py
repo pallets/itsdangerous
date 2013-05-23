e0ba072367aee910f3e1254fdd5ea70da44fcc16
Armin Ronacher <armin.ronacher@active-4.com>
2013-05-23 14:13:34 +0100

    This is 0.20

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='itsdangerous',
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
-    version='0.19',
+    version='0.20',
     url='http://github.com/mitsuhiko/itsdangerous',
     py_modules=['itsdangerous'],
     description='Various helpers to pass trusted data to '
