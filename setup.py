811f63b66720d1312e369dd94969c3685267ef72
Hsiaoming Yang <me@lepture.com>
2018-04-19 23:41:37 +0900

    Drop testing for 2.6 and 3.3. Add 3.6.
    Via discussion https://github.com/pallets/meta/issues/24

--- setup.py
+++ setup.py
@@ -11,9 +11,11 @@ with open(os.path.join(setup_dir, 'itsdangerous.py'), 'rb') as f:
 setup(
     name='itsdangerous',
     version=version,
+    url='https://www.palletsprojects.com/p/itsdangerous/',
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
-    url='https://github.com/pallets/itsdangerous',
+    maintainer='Pallets team',
+    maintainer_email='contact@palletsprojects.com',
     description='Various helpers to pass trusted data to untrusted environments and back.',
     py_modules=['itsdangerous'],
     include_package_data=True,
@@ -26,11 +28,10 @@ setup(
         'Operating System :: OS Independent'
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
